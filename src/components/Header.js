a597fbc9958e20c73a1a078392441a6395b51231
halo <cembreyfarquhar@gmail.com>
2019-09-14 15:46:35 -0500

    styling changes, mainly spacing of the header components and adjusting font sizes in every component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Profile from "./Profile";
 
 function Header() {
   return (
     <header className="header">
       <h1>Paperspace changelog</h1>
+      <Profile />
     </header>
   );
 }
---
+++ src/components/Profile.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Profile() {
+    return (
+        <div className="profile-icon">
+            <h5>User</h5>
+        </div>
+    )
+}
+
+export default Profile;
