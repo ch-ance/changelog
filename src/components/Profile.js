5772732b8267b0b3ea1ca2d4cab2d961f69a51aa
halo <cembreyfarquhar@gmail.com>
2019-09-14 16:46:50 -0500

    user icon has notifications overlay

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUserCircle } from "@fortawesome/free-solid-svg-icons";
 
 function Profile() {
-    return (
-        <div className="profile-icon">
-            <h5>User</h5>
-        </div>
-    )
+  return (
+    <div className="profile-icon">
+      <FontAwesomeIcon icon={faUserCircle} />
+      <NotificationIcon number={5} />
+    </div>
+  );
+}
+
+function NotificationIcon({ number }) {
+  return <span className="notify">{number}</span>;
 }
 
 export default Profile;
