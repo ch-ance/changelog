1aa04b69939c3b36452b2c4396bde49d4f9a477f
halo <cembreyfarquhar@gmail.com>
2019-09-15 11:49:13 -0500

    adds a header for the modal changelog

--- src/components/ModalChangeLog.js
+++ src/components/ModalChangeLog.js
@@ -4,6 +4,10 @@ import ModalChange from "./ModalChange";
 function ModalChangeLog({ changes, clearNotification }) {
   return (
     <div className="modal-changelog">
+      <div className="modal-header">
+      <span>{"<=="}</span>
+      <h4>Latest Changes</h4>
+      </div>
       {changes.map(change => {
         return (
           <ModalChange change={change} clearNotification={clearNotification} />
