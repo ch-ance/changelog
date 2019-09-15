b263c73c89bc3387146f62505a02c87ce3ebbc54
halo <cembreyfarquhar@gmail.com>
2019-09-15 17:38:15 -0500

    dries up the code

--- src/components/ModalChangeLog.js
+++ src/components/ModalChangeLog.js
@@ -4,31 +4,9 @@ import ModalChange from "./ModalChange";
 function ModalChangeLog({ changes, clearNotification }) {
   const [extraView, setExtraView] = useState(false);
 
-  if (extraView) {
-    return (
-      <div className="modal-changelog">
-        <div className="modal-header">
-          <h4>Latest Changes</h4>
-        </div>
-        {changes.map(change => {
-          return (
-            <ModalChange
-              change={change}
-              clearNotification={clearNotification}
-              extraView={extraView}
-              setExtraView={setExtraView}
-              extra
-            />
-          );
-        })}
-      </div>
-    );
-  }
-
   return (
     <div className="modal-changelog">
       <div className="modal-header">
-        <span>{"<=="}</span>
         <h4>Latest Changes</h4>
       </div>
       {changes.map(change => {
@@ -38,6 +16,7 @@ function ModalChangeLog({ changes, clearNotification }) {
             clearNotification={clearNotification}
             extraView={extraView}
             setExtraView={setExtraView}
+            extra={extraView}
           />
         );
       })}
