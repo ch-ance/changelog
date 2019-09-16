619941a332dd7866de386abee9ffd819f308a6e6
halo <cembreyfarquhar@gmail.com>
2019-09-16 01:21:17 -0500

    adds company logo. chore: refactor code for readability

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,21 @@
 import React from "react";
 import Profile from "./Profile";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCreativeCommonsPdAlt } from "@fortawesome/free-brands-svg-icons";
 
 function Header({ openModal, notifNumber }) {
   return (
     <div className="header-container">
       <header className="header">
-        <h1>Paperspace changelog</h1>
+        <div className="left-side">
+          <FontAwesomeIcon className="logo" icon={faCreativeCommonsPdAlt} />
+          <div className="header-title">
+            <h1>
+              Paperspace <em>changelog</em>
+            </h1>
+            <a href="https://www.paperspace.com">paperspace.com</a>
+          </div>
+        </div>
         <Profile openModal={openModal} notifNumber={notifNumber} />
       </header>
     </div>
